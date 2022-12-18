7f4d5ed5031a30d4ac60f501be24fd2a4bb15c3c
Pine <pine.zhang@smartpay.co.nz>
2022-12-19 09:40:50 +1300

    Add application

--- lib/src/main/java/com/pine/lib/net/NetworkUtils.kt
+++ lib/src/main/java/com/pine/lib/net/NetworkUtils.kt
@@ -1,6 +1,5 @@
 package com.pine.lib.net
 
-import android.annotation.SuppressLint
 import android.content.Context
 
 import android.net.wifi.WifiManager
@@ -9,15 +8,20 @@ import com.pine.lib.app.c
 
 object NetworkUtils {
     fun getAddressLog(port: Int): String {
-        val wifiManager = c().applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val formattedIpAddress = getIpAddress()
+        return "Open http://$formattedIpAddress:$port in your browser"
+    }
+
+    fun getIpAddress(): String {
+        val wifiManager =
+            c().applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         val ipAddress = wifiManager.connectionInfo.ipAddress
-        @SuppressLint("DefaultLocale") val formattedIpAddress = String.format(
+        return String.format(
             "%d.%d.%d.%d",
             ipAddress and 0xff,
             ipAddress shr 8 and 0xff,
             ipAddress shr 16 and 0xff,
             ipAddress shr 24 and 0xff
         )
-        return "Open http://$formattedIpAddress:$port in your browser"
     }
 }
