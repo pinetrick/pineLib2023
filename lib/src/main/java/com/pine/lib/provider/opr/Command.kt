9f311aacb3a4eceda1077c82083a9ceb577b2b11
Pine <pine.zhang@smartpay.co.nz>
2022-12-22 15:07:41 +1300

    Query

--- lib/src/main/java/com/pine/lib/provider/opr/Command.kt
+++ lib/src/main/java/com/pine/lib/provider/opr/Command.kt
@@ -3,13 +3,16 @@ package com.pine.lib.provider.opr
 import com.pine.lib.addone.app_broadcast.AppBroadcastController
 
 class Command : BaseOpr() {
-    fun run(){
-        AppBroadcastController.reg("lib_command") { key, value ->
+    fun run() {
+        AppBroadcastController.send("lib_command", requestData.args)
+    }
+
+
+}
+
+//AppBroadcastController.reg("lib_command") { key, value ->
 //            val className = Class.forName("nz.co.smartpay.artoo.ui.home.HomeScreenVM") as Class<ViewModel>
 //            val viewModel = ViewModelProvider(owner = a() as ViewModelStoreOwner)[className]
 //            val method: Method = className.getMethod("onValueAppended", String::class.java)
 //            method.invoke("4")
-        }
-        AppBroadcastController.send("lib_command", requestData.args)
-    }
-}
+//}
